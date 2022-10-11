301cdccbf8e4fda95aa7b951b5f3429a6db50db1
bhargav.akbari <bhargav@dream11.com>
2022-10-11 20:26:39 +0530

    feat: js logger added

---
+++ Libraries/dreamlog.js
@@ -0,0 +1,4 @@
+function dreamLog(className, tag, threadName) {
+  console.log("D11CustomLogManual ===> name:" + className +
+              " ===> extraInfo:" + tag +  " ===> thread:" + threadName + " ===> time:" + Date.now()))
+}
